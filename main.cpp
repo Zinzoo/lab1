f0bd8adf5a7dbf8d9a8c7de3c1f1b8ffacddfa0d
Zinzoo <frytel666@gmail.com>
2016-03-20 14:11:51 +0100

    Ziemniaki

--- main.cpp
+++ main.cpp
@@ -8,16 +8,30 @@ void drawCharSquare(char **square, int n);
 int main(int argc, char **argv)
 {
 	int n;
+	n=atoi(argv[1]);
+	srand(atoi(argv[2]));
 	drawCharSquare(charSquare(n), n);	
 }
 
 char **charSquare(int n)
 {
-	char **square;
+	char **square=new char*[n];
+	for(int i=0; i<n; i++)
+	{
+		square[i]= new char [n];
+		for(int j=0; j<n; j++)
+			square[i][j]=rand() %('z'-'a')+'a';
+	}
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
-
+	for(int i=0; i<n; i++)
+		for(int j=0; j<n; j++)
+		{
+			printf("%c ", square[i][j]);
+			if(j==(n-1))
+				printf("\n");
+		}
 }
